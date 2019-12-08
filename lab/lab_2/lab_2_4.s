7dc7d70594087a7df26d5213caa8216997bf804f
Mattia <sokom141@gmail.com>
2019-12-08 14:32:33 +0100

    lab: 2

---
+++ lab/lab_2/lab_2_4.s
@@ -0,0 +1,21 @@
+@ Calcolo quoziente e resto della divisione m/n
+
+.data
+    mn: .word   26, 12           @ m, n. Numeri interi positivi
+
+.text
+main:   LDR R3, =mn             @ carico l'array
+        LDR R1, [R3]            @ carico nel registro R1 m
+        LDR R2, [R3, #4]        @ carico nel registro R5 n
+
+rem:    CMP R1, R2              @ Aggiorno il registro di stato
+        BGE div                 @ Se m >= n branch alla divsione
+        B stop                  @ Branch a software interrupt
+
+div:    SUB R1, R1, R2         @ m - n
+        ADD R0, R0, #1         @ quoziente + 1
+        B rem                  
+
+stop:   SWI 0x11
+
+.end
